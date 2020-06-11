063cc59daaa4667b9096fa407aa85438a1d2143f
a-name-not-used <wangxin_0716@me.com>
2020-06-12 04:39:58 +0800

    debug.

--- get_tasks.py
+++ get_tasks.py
@@ -33,17 +33,21 @@ def get_few_shot_tasksets(
     """
     if dataset == 'mini-imagenet':
         train_transform = transforms.Compose([
-            transforms.ToPILImage(mode='RGB')
-            transforms.Resize(160, interpolation=Image.BILINEAR),
-            transforms.RandomCrop(128),
+            transforms.ToPILImage(mode='RGB'),
+            # transforms.Resize(160, interpolation=Image.BILINEAR),
+            # transforms.RandomCrop(128),
+            transforms.RandomResizedCrop(128),
+            transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
             # transforms.RandomHorizontalFlip(),
             transforms.ToTensor()
         ])
 
         test_transform = transforms.Compose([
-            transforms.ToPILImage(mode='RGB')
-            transforms.Resize(160, interpolation=Image.BILINEAR),
-            transforms.CenterCrop(128),
+            transforms.ToPILImage(mode='RGB'),
+            # transforms.Resize(160, interpolation=Image.BILINEAR),
+            # transforms.CenterCrop(128),
+            transforms.Resize(128),
+            transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4),
             transforms.ToTensor()
         ])
     else:
