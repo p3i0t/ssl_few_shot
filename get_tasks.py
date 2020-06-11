dce00e9cea72589b03c2622579d1f887617d7270
a-name-not-used <wangxin_0716@me.com>
2020-06-12 00:09:21 +0800

    refactor

---
+++ get_tasks.py
@@ -0,0 +1,170 @@
+from torchvision import transforms
+
+import learn2learn as l2l
+from PIL import Image
+
+from learn2learn.data.transforms import NWays, KShots, LoadData, RemapLabels, ConsecutiveLabels
+
+from collections import namedtuple
+BenchmarkTasksets = namedtuple('BenchmarkTasksets', ('train', 'validation', 'test'))
+
+
+def get_few_shot_tasksets(
+        root='data',
+        dataset='cifar10-fc100',
+        train_ways=5,
+        train_samples=10,
+        test_ways=5,
+        test_samples=10,
+        n_train_tasks=2000,
+        n_test_tasks=1000
+    ):
+    """
+    Fetch the train, valid, test meta tasks of given dataset.
+    :param root: data directory.
+    :param dataset: name of dataset.
+    :param train_ways: number of ways of few-shot training.
+    :param train_samples: number of each-way samples for a training task.
+    :param test_ways: number of ways of few-shot evaluation and testing.
+    :param test_samples: number of each-way samples for a valid or test task.
+    :param n_train_tasks: total number of train tasks.
+    :param n_test_tasks: total number of valid and test tasks.
+    :return:
+    """
+    train_transform = transforms.Compose([
+        transforms.Resize(160, interpolation=Image.BILINEAR),
+        transforms.RandomCrop(128),
+        # transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+    ])
+
+    test_transform = transforms.Compose([
+        transforms.Resize(160, interpolation=Image.BILINEAR),
+        transforms.CenterCrop(128),
+        transforms.ToTensor(),
+    ])
+
+    if dataset == 'cifar10-fs':
+        train_dataset = l2l.vision.datasets.CIFARFS(
+            root=root,
+            transform=train_transform,
+            mode='train'
+        )
+        valid_dataset = l2l.vision.datasets.CIFARFS(
+            root=root,
+            transform=test_transform,
+            mode='validation'
+        )
+        test_dataset = l2l.vision.datasets.CIFARFS(
+            root=root,
+            transform=test_transform,
+            mode='test'
+        )
+    elif dataset == 'cifar10-fc100':
+        train_dataset = l2l.vision.datasets.FC100(
+            root=root,
+            transform=train_transform,
+            mode='train'
+        )
+        valid_dataset = l2l.vision.datasets.FC100(
+            root=root,
+            transform=test_transform,
+            mode='validation'
+        )
+        test_dataset = l2l.vision.datasets.CIFARFS(
+            root=root,
+            transform=test_transform,
+            mode='test'
+        )
+    elif dataset == 'mini-imagenet':
+        train_dataset = l2l.vision.datasets.MiniImagenet(
+            root=root,
+            transform=train_transform,
+            mode='train'
+        )
+        valid_dataset = l2l.vision.datasets.MiniImagenet(
+            root=root,
+            transform=test_transform,
+            mode='validation'
+        )
+        test_dataset = l2l.vision.datasets.MiniImagenet(
+            root=root,
+            transform=test_transform,
+            mode='test'
+        )
+    elif dataset == 'tiered-imagenet':
+        train_dataset = l2l.vision.datasets.TieredImagenet(
+            root=root,
+            transform=train_transform,
+            mode='train'
+        )
+        valid_dataset = l2l.vision.datasets.TieredImagenet(
+            root=root,
+            transform=test_transform,
+            mode='validation'
+        )
+        test_dataset = l2l.vision.datasets.TieredImagenet(
+            root=root,
+            transform=test_transform,
+            mode='test'
+        )
+    else:
+        raise Exception("dataset {} not available.".format(dataset))
+
+    train_dataset = l2l.data.MetaDataset(train_dataset)
+    valid_dataset = l2l.data.MetaDataset(valid_dataset)
+    test_dataset = l2l.data.MetaDataset(test_dataset)
+
+    train_transforms = [
+        NWays(train_dataset, train_ways),
+        KShots(train_dataset, train_samples),
+        LoadData(train_dataset),
+        RemapLabels(train_dataset),
+        ConsecutiveLabels(train_dataset),
+    ]
+    valid_transforms = [
+        NWays(valid_dataset, test_ways),
+        KShots(valid_dataset, test_samples),
+        LoadData(valid_dataset),
+        ConsecutiveLabels(valid_dataset),
+        RemapLabels(valid_dataset),
+    ]
+    test_transforms = [
+        NWays(test_dataset, test_ways),
+        KShots(test_dataset, test_samples),
+        LoadData(test_dataset),
+        RemapLabels(test_dataset),
+        ConsecutiveLabels(test_dataset),
+    ]
+
+    # Instantiate the tasksets
+    train_tasks = l2l.data.TaskDataset(
+        dataset=train_dataset,
+        task_transforms=train_transforms,
+        num_tasks=n_train_tasks,
+    )
+    validation_tasks = l2l.data.TaskDataset(
+        dataset=valid_dataset,
+        task_transforms=valid_transforms,
+        num_tasks=n_test_tasks,
+    )
+    test_tasks = l2l.data.TaskDataset(
+        dataset=test_dataset,
+        task_transforms=test_transforms,
+        num_tasks=n_test_tasks,
+    )
+
+    return BenchmarkTasksets(train_tasks, validation_tasks, test_tasks)
+
+
+if __name__ == '__main__':
+    tasks = get_few_shot_tasksets(dataset='cifar10-fs')
+    # batch = tasks.train.sample()
+    # x, y = batch
+    # print(x.size())
+    # print(y.size())
+    # print(y)
+    x, y = tasks.train[0]
+    print(x.size())
+    print(y.size())
+
---
+++ main.py
@@ -0,0 +1,148 @@
+import hydra
+from omegaconf import DictConfig
+import logging
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn.functional as F
+import learn2learn as l2l
+
+from get_tasks import get_few_shot_tasksets
+
+import pytorch_lightning as pl
+
+
+class FewShotLearner(pl.LightningModule):
+    def __init__(self, backbone='resnet50x1', root='data', dataset='cifar10-fc100'):
+        super().__init__()
+
+        assert dataset in ['cifar10-fc100', 'cifar10-fs', 'mini-imagenet', 'tiered-imagenet']
+
+        self.backbone = eval(backbone)()
+        if backbone == 'resnet50x1':
+            checkpoint_path = 'resnet50-1x.pth'
+        elif backbone == 'resnet50x2':
+            checkpoint_path = 'resnet50-2x.pth'
+        elif backbone == 'resnet50x4':
+            checkpoint_path = 'resnet50-4x.pth'
+
+        state = torch.load(checkpoint_path, map_location='cpu')
+        self.backbone.load_state_dict(state['state_dict'])
+
+        self.proj_dim = self.backbone.fc.out_features
+
+        self.n_ways = 5
+        self.n_shots = 1
+        self.n_queries = 6
+        self.n_aug_support = 1
+
+        self.n_train_tasks = 2000
+        self.n_test_tasks = 1000
+
+        self.tasksets = get_few_shot_tasksets(
+            root=root,
+            dataset=dataset,
+            train_ways=self.n_ways,
+            test_ways=self.n_ways,
+            train_samples=self.n_shots + self.n_queries,
+            test_samples=self.n_shots + self.n_queries,
+            n_train_tasks=self.n_train_tasks,
+            n_test_tasks=self.n_test_tasks
+        )
+
+    def forward(self, x):
+        b, prod, c, h, w = x.size()  # prod = n_way * n_aug
+        return F.normalize(self.backbone(x.view(-1, c, h, w)), dim=-1)
+
+    def _batch_forward(self, batch):
+        x, y = batch
+        b, way_shot_query, c, h, w = x
+
+        x_ = x.view(b, self.n_ways, (self.n_shots + self.n_queries), c, h, w)
+        y_ = y.view(b, self.n_ways, (self.n_shots + self.n_queries))
+
+        x_support, x_queries = torch.split_with_sizes(x_, split_sizes=[self.n_shots, self.n_queries], dim=2)
+        y_support, y_queries = torch.split_with_sizes(y_, split_sizes=[self.n_shots, self.n_queries], dim=2)
+
+        rep_s = self(x_support.view(b, -1, c, h, w))
+        rep_q = self(x_queries.view(b, -1, c, h, w))
+
+        q = rep_q.view(b, self.n_ways * self.n_queries, self.proj_dim)
+        # centroid of same way/class
+        s = rep_s.view(b, self.n_ways, self.n_shots * self.n_aug_support, self.proj_dim).mean(dim=2)
+        s = s.clone().permute(0, 2, 1).contiguous()
+
+        cosine_scores = q @ s  # batch matrix multiplication
+        logits = cosine_scores.view(-1, self.n_ways) / 0.1
+        labels = y_queries.view(-1)
+
+        loss = F.cross_entropy(logits, labels)
+        acc = (logits.argmax(dim=1) == labels).float().mean()
+        return loss, acc
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.001)
+
+    def train_dataloader(self):
+        train_loader = torch.utils.data.DataLoader(
+            dataset=self.tasksets.train,
+            batch_size=20,
+            drop_last=False,
+        )
+        return train_loader
+
+    def training_step(self, batch, batch_idx):
+        loss, acc = self._batch_forward(batch)
+        return {'loss': loss, 'acc': acc}
+
+    def training_epoch_end(self, outputs):
+        # OPTIONAL
+        avg_loss = torch.stack([x['loss'] for x in outputs]).mean()
+
+        avg_acc = torch.stack([x['acc'] for x in outputs]).mean()
+
+        logs = {'avg_loss': avg_loss, 'avg_acc': avg_acc}
+
+        return {'avg_loss': avg_loss, 'avg_acc': avg_acc, 'log': logs, 'progress_bar': logs}
+
+    def val_dataloader(self):
+        val_loader = torch.utils.data.DataLoader(
+            dataset=self.tasksets.validation,
+            batch_size=20,
+            drop_last=False,
+        )
+        return val_loader
+
+    def test_dataloader(self):
+        test_loader = torch.utils.data.DataLoader(
+            dataset=self.tasksets.test,
+            batch_size=20,
+            drop_last=False,
+        )
+        return test_loader
+
+    def test_step(self,  batch, batch_idx):
+        loss, acc = self._batch_forward(batch)
+        return {'test_loss': loss, 'test_acc': acc}
+
+    def test_end(self, outputs):
+        # OPTIONAL
+        avg_loss = torch.stack([x['test_loss'] for x in outputs]).mean()
+
+        avg_acc = torch.stack([x['test_acc'] for x in outputs]).mean()
+
+        logs = {'test_loss': avg_loss, 'test_acc': avg_acc}
+
+        return {'avg_test_loss': avg_loss, 'avg_test_acc': avg_acc, 'log': logs, 'progress_bar': logs}
+
+
+if __name__ == '__main__':
+    fewshot_learner = FewShotLearner(backbone='resnet50x1')
+    trainer = pl.Trainer(
+        gpus=2,
+        max_epochs=1,
+        distributed_backend='ddp',
+        precision=16,
+    )
+    trainer.fit(fewshot_learner)
+    trainer.test()
